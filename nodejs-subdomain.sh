7b2293c5df5dd486dd3f6d1d4b5d509cf24864f2
gijslaarman <gijslaarman@live.nl>
2020-04-11 21:58:53 +0200

    Added some error handling

--- nodejs-subdomain.sh
+++ nodejs-subdomain.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+# Script to create new subdomains for your NodeJS applications.
+# Inspired & copyed some bits from: https://github.com/karakanb/nginx-subdomain-generator-script
+
 ok() { echo -e '\e[32m'$1'\e[m'; } # Green
+die() { echo -e '\e[1;31m'$1'\e[m'; exit 1; }
 
 # Variable definitions
 DOMAIN=$1
 PORT=$2
 NGINX_AVAILABLE_SITES='/etc/nginx/sites-available'
 NGINX_ENABLED_SITES='/etc/nginx/sites-enabled'
+domainRegex='^([a-zA-Z0-9]([-a-zA-Z0-9]{0,61}[a-zA-Z0-9])?\.)?([a-zA-Z0-9]{1,2}([-a-zA-Z0-9]{0,252}[a-zA-Z0-9])?)\.([a-zA-Z]{2,63})$'
+portRegex='^[0-9]+$'
+
+# Error handling
+[ $(id -g) != "0"] && die "Must run as root"
+[ $# != "2" ] && die "Usage: $(basename $0) sub.domain.name portnumber"
+if [[ ! $DOMAIN =~ $domainRegex ]]; then die "$DOMAIN is not a valid subdomainname."; fi
+if [[ ! $PORT =~ $portRegex ]]; then die "$PORT is not a number"; fi
 
 # Create the Nginx config file.
 cat > $NGINX_AVAILABLE_SITES/$DOMAIN <<EOF
