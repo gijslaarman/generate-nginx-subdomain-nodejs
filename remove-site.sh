ae9a469c74e3ea3a56e362d59bd97ead314bd05d
gijslaarman <gijslaarman@live.nl>
2020-04-11 20:18:20 +0200

    remove site prompt

--- remove-site.sh
+++ remove-site.sh
@@ -8,7 +8,7 @@ DOMAIN_NAME=$1
 rm $NGINX_AVAILABLE_SITES/$DOMAIN_NAME
 rm $NGINX_ENABLED_SITES/$DOMAIN_NAME
 
-read -p "A restart to Nginx is required for the subdomain to be defined. Do you wish to restart nginx? (y/$
+read -p "A restart to Nginx is required for the subdomain to be defined. Do you wish to restart nginx? (y/n): " prompt
 if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
 then
   /etc/init.d/nginx restart;
